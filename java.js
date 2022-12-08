24de8baaac3f94a6a6b5db0c0d6c25fe8359a6bd
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2022-12-08 16:10:55 +0100

    modified:   index.html 	modified:   java.js 	new file:   media/Print-mockup.png 	modified:   style.css

--- java.js
+++ java.js
@@ -3,3 +3,9 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
   console.log("siden vises");
 }
+
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
