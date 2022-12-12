a41e4f31cc52021f16c7f6bb8a64fdc906c55cd6
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2022-12-12 18:25:11 +0100

    modified:   index.html 	modified:   java.js 	modified:   style.css

--- java.js
+++ java.js
@@ -128,4 +128,5 @@ function menuOnClick() {
   document.getElementById("menu-bar").classList.toggle("change");
   document.getElementById("nav").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
+  document.getElementById("remove").classList.toggle("change-bg");
 }
